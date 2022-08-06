e6aeef2424039d54efd5b5a751f3e76079c3fe69
MohammadReza Taheri <taheri.mr73@gmail.com>
2022-08-06 20:44:03 +0430

    create Components and Containers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Grid, Typography } from "@mui/material";
+import "./App.css";
+import Faq from "./components/Faq";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <Grid container spacing={5}>
+        <Grid item>
+          <Typography mt={10}>
+            Coding Challenge - Frontend Developer - DELTABAN
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Faq />
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
